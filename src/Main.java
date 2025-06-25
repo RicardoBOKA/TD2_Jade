30fdacbeeb0dd5990c0e8ce4d821031e2d6e1251
RicardoBOKA <104567739+RicardoBOKA@users.noreply.github.com>
2025-06-25 10:37:13 +0200

    Add main class to launch all JADE agents

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.StaleProxyException;
+
 /**
- * Classe de test affichant simplement un message dans la console.
+ * Classe principale permettant d'exécuter l'ensemble des agents de
+ * l'application. Elle crée un conteneur JADE et lance chacun des agents
+ * définis dans le projet.
  */
 public class Main {
     /**
-     * Point d'entrée de l'application de test.
+     * Point d'entrée de l'application.
+     *
+     * <p>Un conteneur JADE est créé puis chaque agent est démarré à son tour.</p>
      */
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Runtime rt = Runtime.instance();
+        Profile p = new ProfileImpl();
+        AgentContainer container = rt.createMainContainer(p);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        try {
+            container.createNewAgent("agent2", Agent2.class.getName(), null).start();
+            container.createNewAgent("agent3", Agent3.class.getName(), null).start();
+            container.createNewAgent("genericAgent", GenericAgent.class.getName(), null).start();
+            container.createNewAgent("myGenericAgent", MyGenericAgent.class.getName(), null).start();
+            container.createNewAgent("myGenericBehaviourAgent2", MyGenericBehaviourAgent2.class.getName(), null).start();
+            container.createNewAgent("printBehaviourAgent", PrintBehaviourAgent.class.getName(), null).start();
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
         }
     }
 }
